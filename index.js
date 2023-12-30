61e2a33c7bf8aa36fb70750c8ec7a2ae8cba1f32
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2023-12-29 19:00:00 -0500

    static keyword

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-class Student {
-    constructor(name, age, gpa) {
-        this.name = name;
-        this.age = age;
-        this.gpa = gpa;
+class Car {
+    // use static keyword
+    static numOfCars = 0;
+
+    constructor(model) {
+        this.model = model;
+        Car.numOfCars ++; // refer to it using Class.property, not this.property,
+        // that is, Car.numOfCars and not this.numOfCars
     }
-    
-    study() {
-        console.log(`${this.name} is studying!`);
+
+    // static method, same idea
+    static startRace() {
+        console.log("3.. 2.. 1.. GO!");
     }
 }
 
-const stud1 = new Student("spongebob", 19, 4.0);
-console.log(stud1.gpa);
+car1 = new Car("honda");
+car2 = new Car("ferrari");
+let car3 = new Car("toyota");
+let car4 = new Car("nissan");
+
+// access the static variable directly from class
+// cannot invoke it from a specific instance
+console.log(Car.numOfCars);
+
+// call static method directly from class
+// cannot invoke it from a specific instance
+Car.startRace();
--- notes.js
+++ notes.js
@@ -1031,3 +1031,34 @@ class Student {
 const stud1 = new Student("spongebob", 19, 4.0);
 console.log(stud1.gpa);
 
+// STATIC KEYWORD
+// static method and properties belong to entire class and not any specific objects 
+
+class Car {
+    // use static keyword
+    static numOfCars = 0;
+
+    constructor(model) {
+        this.model = model;
+        Car.numOfCars ++; // refer to it using Class.property, not this.property,
+        // that is, Car.numOfCars and not this.numOfCars
+    }
+
+    // static method, same idea
+    static startRace() {
+        console.log("3.. 2.. 1.. GO!");
+    }
+}
+
+car1 = new Car("honda");
+car2 = new Car("ferrari");
+let car3 = new Car("toyota");
+let car4 = new Car("nissan");
+
+// access the static variable directly from class
+// cannot invoke it from a specific instance
+console.log(Car.numOfCars);
+
+// call static method directly from class
+// cannot invoke it from a specific instance
+Car.startRace();
