d07787e658e7b03a75ea5e9709cb10bdf460af79
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2023-12-30 21:54:43 -0500

    js events

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-const title = document.getElementById("helloworld");
+// events are user interactions etc.
+// for instance, onclick event
 
-title.style.backgroundColor = "lightgreen";
-// we say element.style.property = value;
+// we can have the HTML code reference the functions here
+function doSomething() {
+    alert("you did something !")
+}
+
+// or we can directly set events here as well
+document.getElementById("myButt").onclick = function () {
+    alert("you clicked the BUTTON!")
+}
+// another event is onload; does the function when website loads in
+
+// another is onchange; does when the element has been changed
+document.getElementById("nameText").onchange = function () {
+    alert("YO!");
+}
+
+let myDiv = document.getElementById("div1");
+
+myDiv.onmouseover = function () {
+    myDiv.style.backgroundColor = "lightgreen";
+}
+
+myDiv.onmouseleave = function () {
+    myDiv.style.backgroundColor = "lightblue";
+}
